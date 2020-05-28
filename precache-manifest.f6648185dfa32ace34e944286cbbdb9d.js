ef7924670cae12111ecccf8c5468bd20ab20a1ec
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-05-28 14:06:24 +0200

    Updates

---
+++ precache-manifest.f6648185dfa32ace34e944286cbbdb9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6366d3cc239f40349470d673290e4f",
+    "url": "/BlogPosts/index.html"
+  },
+  {
+    "revision": "c6f117edd9f4fcb7bfa7",
+    "url": "/BlogPosts/static/js/2.15c82ec8.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/BlogPosts/static/js/2.15c82ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e9de59e4fe735ee20f",
+    "url": "/BlogPosts/static/js/main.8c1bc414.chunk.js"
+  },
+  {
+    "revision": "818a453cdfe5027f9f90",
+    "url": "/BlogPosts/static/js/runtime-main.903ee36a.js"
+  }
+]);
